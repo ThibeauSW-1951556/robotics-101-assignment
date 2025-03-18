43cea338a35d6149c2473789a82526d5e2e97e1e
Dries Cardinaels <143814887+Dries-Cardinaels@users.noreply.github.com>
2025-03-18 10:28:54 +0100

    Update RobotController.cs (Minor Bugfixes)

--- robotics-101/Assets/Robotic Arm/Scripts/RobotController.cs
+++ robotics-101/Assets/Robotic Arm/Scripts/RobotController.cs
@@ -1,8 +1,8 @@
 using System;
+using System.Globalization;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using Unity.VisualScripting;
 using UnityEngine;
 public class RobotController
 {
